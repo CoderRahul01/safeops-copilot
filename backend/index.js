3531cad9f5fe15e9edb0ad7593942a82bb0e317d
Rahul Pandey <rahulprogrammerrp@gmail.com>
2026-02-04 20:20:01 +0530

    Modification in frontend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,3 @@
-const fastify = require('fastify')({ logger: true });
-fastify.get('/', async () => ({ status: 'SafeOps Backend Ready' }));
-fastify.listen({ port: process.env.PORT || 8080 });
+const fastify = require("fastify")({ logger: true });
+fastify.get("/", async () => ({ status: "SafeOps Backend Ready" }));
+fastify.listen({ port: process.env.PORT || 8080, host: "0.0.0.0" });
